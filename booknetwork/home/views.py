0645356f711300e6b42827ff2361af23d3c197c3
Johnathan Ingle <jingle1000@gmail.com>
2018-12-28 13:56:08 -0500

    Update Layout

--- booknetwork/home/views.py
+++ booknetwork/home/views.py
@@ -2,5 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+books = [
+    {
+        'isbn': 'xxxxxxxxxxxxxx',
+        'title': 'A Book Title',
+        'author': 'Bob Smith',
+        'year_published': '2018'
+    },
+    {
+        'isbn': 'xxxxxxxxxxxxxx',
+        'title': 'A Book Title',
+        'author': 'Bob Smith',
+        'year_published': '2018'
+    },
+    {
+        'isbn': 'xxxxxxxxxxxxxx',
+        'title': 'A Book Title',
+        'author': 'Bob Smith',
+        'year_published': '2018'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Book Reviews</h1>')
+    content = {
+        'books': books
+    }
+    return render(request, 'home/layout.html', content)
