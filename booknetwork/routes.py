0b0341965e276f4f19e195b5312c1937eca06199
Johnathan Ingle <jingle1000@gmail.com>
2019-01-01 13:57:28 -0500

    Working Search Function

--- booknetwork/routes.py
+++ booknetwork/routes.py
@@ -9,8 +9,34 @@ import datetime
 #create database schema
 Base.metadata.create_all(engine)
 
-@app.route('/')
+search_data = []
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
     form = SearchForm()
-    return render_template('index.html', form=form)
+    if request.method == "POST":
+        text = form.searchText.data
+        result = db.execute(
+            "SELECT * FROM book WHERE (LOWER(isbn) LIKE LOWER(:text)) OR (LOWER(title) LIKE LOWER(:text)) OR (author LIKE LOWER(:text)) LIMIT 10",
+            { "text": '%' + text + '%'} 
+        ).fetchall()
+        for book in result:
+            book_fields = {}
+            book_fields['isbn'] = book[1]
+            book_fields['title'] = book[2]
+            book_fields['author'] = book[3]
+            book_fields['year_published'] = book[4]
+            search_data.append(book_fields)
+        return redirect(url_for('search_results'))
+    return render_template('index.html', form=form, dashboard=True)
+
+@app.route('/searchresults')
+def search_results():
+    print('running search')
+    print(search_data)
+    return render_template('searchresults.html', results=search_data)
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for('home'))
 
